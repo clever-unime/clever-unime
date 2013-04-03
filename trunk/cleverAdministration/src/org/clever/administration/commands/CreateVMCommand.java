b5a80b32b52ebcc961cd730ecb04a86866ddebda
trigiu <giu.tricomi@gmail.com>
2013-04-03 13:43:44 +0300

    Update CreateVMCommand.java
    now is possible make VM with disk image exclusive or clonable.

--- trunk/cleverAdministration/src/org/clever/administration/commands/CreateVMCommand.java
+++ trunk/cleverAdministration/src/org/clever/administration/commands/CreateVMCommand.java
@@ -20,13 +20,14 @@ public class CreateVMCommand extends CleverCommand{
     @Override
     public Options getOptions() {
         Options options = new Options();
-         options.addOption( "n", true, "The name of the Virtual Machine." );
+         options.addOption( "tn", true, "The template name of the Virtual Machine." );
          options.addOption( "h", true, "The name of the host manager." );
+         //options.addOption( "n", true, "The name of the Virtual Machine." );
          //options.addOption( "lock", true, "[0-NL] [1-CR] [2-CW] [3-PR] [4-PW] [5-EX]." ); // tipi di lock
-    //     options.addOption( "clonable", false, "Virtual Machine is cloned." );
-   //      options.addOption( "exclusive", false, "Virtual Machine is exclusive." ); 
+         options.addOption( "clonable", false, "Virtual Machine is cloned." );
+         options.addOption( "exclusive", false, "Virtual Machine is exclusive." ); 
          options.addOption( "debug", false, "Displays debug information." );
-      //   options.addOption( "xml", false, "Displays the XML request/response Messages." );
+         //   options.addOption( "xml", false, "Displays the XML request/response Messages." );
          return options; 
     }
 
@@ -37,20 +38,23 @@ public class CreateVMCommand extends CleverCommand{
         String target = ClusterManagerAdministrationTools.instance().getConnectionXMPP().getActiveCC(ConnectionXMPP.ROOM.SHELL);
         String targetHM=commandLine.getOptionValue("h");
         ArrayList params = new ArrayList();
-        String id=commandLine.getOptionValue("n");
-        int lock=5;
-   /*       if(commandLine.hasOption("clonable")){
-              lock=3;
+        String id=commandLine.getOptionValue("tn");
+        String istanceName=commandLine.getOptionValue("n");
+        org.clever.HostManager.ImageManagerPlugins.ImageManagerClever.LockFile.lockMode lock=org.clever.HostManager.ImageManagerPlugins.ImageManagerClever.LockFile.lockMode.EX;
+          if(commandLine.hasOption("clonable")){
+              lock=org.clever.HostManager.ImageManagerPlugins.ImageManagerClever.LockFile.lockMode.PR;
               
           }
           if(commandLine.hasOption("exclusive")){
-              lock=5;
-          }*/
+              lock=org.clever.HostManager.ImageManagerPlugins.ImageManagerClever.LockFile.lockMode.EX;
+          }
         
         //int lock=Integer.parseInt( commandLine.getOptionValue("lock"));
         params.add(id);
+        //params.add(istanceName);
         params.add(targetHM);
-        params.add(lock);
+        org.clever.Common.Communicator.Utils.IstantiationParams containerParams=new org.clever.Common.Communicator.Utils.IstantiationParams(lock);
+        params.add(containerParams);
         ClusterManagerAdministrationTools.instance().execAdminCommand(this, target, "VirtualizationManagerAgent", "createVM", params, commandLine.hasOption( "xml" ) );
         //ClusterManagerAdministrationTools.instance().execAdminCommand(this, target, "TestAgent", "simVirtualizationManager", params, commandLine.hasOption( "xml" ) );
            }
@@ -61,6 +65,7 @@ public class CreateVMCommand extends CleverCommand{
             else
                 System.out.println(ex);
         }    }
+
 
     @Override
     public void handleMessage(Object response) {
