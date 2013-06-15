25135a697a5ea82e4db78e1ae419c6f9343e10c4
Maurizio <maurizio.paone@gmail.com>
2013-06-15 11:07:59 +0200

    corretto run.sh clevercloud

--- trunk/clevercloud/run.sh
+++ trunk/clevercloud/run.sh
@@ -1,5 +1,5 @@
 OCCIDIR=$(pwd)/../clever-occi-plugins/
 librerie=${OCCIDIR}/lib/*.jar
 librerie=$(echo $librerie|tr ' ' :)
-echo "Lancio: java -cp $librerie:dist/clever.jar:${OCCI}/dist/clever-occi-plugins.jar org.clever.Common.Initiator.Main"
-java -cp $librerie:dist/clever.jar:${OCCI}/dist/clever-occi-plugins.jar org.clever.Common.Initiator.Main
+echo "Lancio: java -cp $librerie:dist/clever.jar:${OCCIDIR}/dist/clever-occi-plugins.jar org.clever.Common.Initiator.Main"
+java -cp $librerie:dist/clever.jar:${OCCIDIR}/dist/clever-occi-plugins.jar org.clever.Common.Initiator.Main
